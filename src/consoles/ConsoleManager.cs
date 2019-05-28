b0fe7eac387d94f924ee994945f9f73602a5118a
cdogyoz <christian.sandford@ttu.edu>
2019-05-28 17:27:12 -0500

    go rogue addition

---
+++ src/consoles/ConsoleManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using SadConsole;
+using SadConsole.Controls;
+
+//Manages all of the consoles used in the game
+
+namespace Timeless_Peach.src.consoles {
+    class ConsoleManager : ContainerConsole {
+
+        public ConsoleManager() {
+            IsVisible = true;
+            IsFocused = true;
+
+            Parent = SadConsole.Global.CurrentScreen;
+        }
+
+    }
+}
