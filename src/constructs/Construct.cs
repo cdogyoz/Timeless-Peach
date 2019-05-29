5c213d73839f4552afef6b8d3f382e885f41c798
cdogyoz <christian.sandford@ttu.edu>
2019-05-29 00:24:10 -0500

    setting up ECS system

--- src/constructs/Construct.cs
+++ src/constructs/Construct.cs
@@ -1,12 +1,20 @@
 ﻿using System;
+using System.Collections.Generic;
 using Microsoft.Xna.Framework;
+using Timeless_Peach.src.components;
 using SadConsole;
 
 namespace Timeless_Peach.src.constructs {
     class Construct : SadConsole.Entities.Entity {
-        
+
+        List<Component> components;
+
         public Construct(Color foreground, Color background, int glyph, Point position) : base(foreground, background, glyph) {
             Position = position;
+        }
+
+        bool FireEvent() {
+            return true;
         }
 
     }
---
+++ src/events/Event.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Timeless_Peach.src.events {
+    class Event {
+        string ID;
+        Dictionary<string, Object> parameters;
+    }
+}
