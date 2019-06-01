fbfb53237586fde60da951a2df30474be650e677
John Pappas <44303481+jaydeepappas@users.noreply.github.com>
2019-06-01 18:22:31 -0500

    Update MainMenuConsole.cs
    Consolidated redundant if/else statements in mainMenuConsole, added quit functionality

--- src/consoles/MainMenuConsole.cs
+++ src/consoles/MainMenuConsole.cs
@@ -41,26 +41,20 @@ namespace Timeless_Peach.src.consoles {
         }
 
         private void CheckInput() {
-            if (SadConsole.Global.KeyboardState.IsKeyPressed(Microsoft.Xna.Framework.Input.Keys.Down)) {
+            if (SadConsole.Global.KeyboardState.IsKeyPressed(Microsoft.Xna.Framework.Input.Keys.Down) || SadConsole.Global.KeyboardState.IsKeyPressed(Microsoft.Xna.Framework.Input.Keys.Up)) {
                 if (selectChoice == 0)
                     selectChoice = 1;
                 else if(selectChoice == 1)
                     selectChoice = 0;
             }
 
-            if (SadConsole.Global.KeyboardState.IsKeyPressed(Microsoft.Xna.Framework.Input.Keys.Up))
-                if (selectChoice == 0)
-                    selectChoice = 1;
-                else if (selectChoice == 1)
-                    selectChoice = 0;
-
-            if (SadConsole.Global.KeyboardState.IsKeyPressed(Microsoft.Xna.Framework.Input.Keys.Space))
+            if (SadConsole.Global.KeyboardState.IsKeyPressed(Microsoft.Xna.Framework.Input.Keys.Space) || SadConsole.Global.KeyboardState.IsKeyPressed(Microsoft.Xna.Framework.Input.Keys.Enter))
                 if (selectChoice == 0)
                     SadConsole.Global.CurrentScreen = conMan.gameScreen;
 
-            if (SadConsole.Global.KeyboardState.IsKeyPressed(Microsoft.Xna.Framework.Input.Keys.Enter))
-                if (selectChoice == 0)
-                    SadConsole.Global.CurrentScreen = conMan.gameScreen;
+            if (SadConsole.Global.KeyboardState.IsKeyPressed(Microsoft.Xna.Framework.Input.Keys.Space) || SadConsole.Global.KeyboardState.IsKeyPressed(Microsoft.Xna.Framework.Input.Keys.Enter))
+                if (selectChoice == 1)
+                    System.Environment.Exit(1);
         }
     }
 }
