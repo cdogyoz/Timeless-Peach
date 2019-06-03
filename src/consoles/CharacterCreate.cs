bf28c314549ec1f7f6badde357e57d24e9067704
John Pappas <44303481+jaydeepappas@users.noreply.github.com>
2019-06-03 12:14:16 -0500

    Update CharacterCreate.cs
    WIP

--- src/consoles/CharacterCreate.cs
+++ src/consoles/CharacterCreate.cs
@@ -10,16 +10,83 @@ namespace Timeless_Peach.src.consoles {
     class CharacterCreate : SadConsole.Console {
 
         private ConsoleManager conMan;
+        private int selectChoice = 0;
+        private string currentRace = "You have not selected a race yet.";
 
         public CharacterCreate(ConsoleManager conMan) : base(80, 25) {
             this.conMan = conMan;
-            Print(33, 0, "Character Create", Color.White, Color.Black );
-            Print(33, 24, "Press enter to play", Color.Black, Color.PeachPuff);
+            Print(33, 0, "Character Create", Color.White, Color.Black);
+            Print(33, 24, "Press space to play", Color.Black, Color.PeachPuff);
+
+            if (selectChoice == 0) {
+                Print(38, 10, "Human", Color.Red);
+                Print(38, 12, "Kimi", Color.White);
+                Print(38, 14, "High Uvese", Color.White);
+                Print(38, 16, "Bronk", Color.White);
+                Print(38, 18, currentRace, Color.PeachPuff);
+            }
+            else if (selectChoice == 1) {
+                Print(38, 10, "Human", Color.White);
+                Print(38, 12, "Kimi", Color.Red);
+                Print(38, 14, "High Uvese", Color.White);
+                Print(38, 16, "Bronk", Color.White);
+                Print(38, 18, currentRace, Color.PeachPuff);
+
+            }
+            if (selectChoice == 2) {
+                Print(38, 10, "Human", Color.White);
+                Print(38, 12, "Kimi", Color.White);
+                Print(38, 14, "High Uvese", Color.Red);
+                Print(38, 16, "Bronk", Color.White);
+                Print(38, 18, currentRace, Color.PeachPuff);
+
+            }
+            else if (selectChoice == 3) {
+                Print(38, 10, "Human", Color.White);
+                Print(38, 12, "Kimi", Color.White);
+                Print(38, 14, "High Uvese", Color.White);
+                Print(38, 16, "Bronk", Color.Red);
+                Print(38, 18, currentRace, Color.PeachPuff);
+
+            }
         }
 
         public override void Update(TimeSpan timeElapsed) {
+            if (SadConsole.Global.KeyboardState.IsKeyPressed(Microsoft.Xna.Framework.Input.Keys.Down)) {
+                if (selectChoice < 3) {
+                    selectChoice++;
+                }
+                else {
+                    selectChoice = 0;
+                }
+            }
+
+            if (SadConsole.Global.KeyboardState.IsKeyPressed(Microsoft.Xna.Framework.Input.Keys.Up)) {
+                if (selectChoice > 0) {
+                    selectChoice--;
+                }
+                else {
+                    selectChoice = 3;
+                }
+            }
+
             if (SadConsole.Global.KeyboardState.IsKeyPressed(Microsoft.Xna.Framework.Input.Keys.Enter)) {
-                Global.CurrentScreen = conMan.gameScreen;
+                if (selectChoice == 0) {
+                    currentRace = "You chose Human.";
+                }
+                if (selectChoice == 1) {
+                    currentRace = "You chose Kimi.";
+                }
+                if (selectChoice == 2) {
+                    currentRace = "You chose High Uvese.";
+                }
+                if (selectChoice == 3) {
+                    currentRace = "You chose Bronk.";
+                }
+            }
+
+            if (SadConsole.Global.KeyboardState.IsKeyPressed(Microsoft.Xna.Framework.Input.Keys.Space)) {
+                SadConsole.Global.CurrentScreen = conMan.gameScreen;
             }
             base.Update(timeElapsed);
         }
