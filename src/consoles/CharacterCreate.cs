61994babfb99d1d693d3bc8bb4740a4b8267e1e5
John Pappas <44303481+jaydeepappas@users.noreply.github.com>
2019-06-03 16:30:25 -0500

    Update CharacterCreate.cs

--- src/consoles/CharacterCreate.cs
+++ src/consoles/CharacterCreate.cs
@@ -5,56 +5,58 @@ using System.Text;
 using System.Threading.Tasks;
 using SadConsole;
 using Microsoft.Xna.Framework;
+using Timeless_Peach.src.components;
 
 namespace Timeless_Peach.src.consoles {
     class CharacterCreate : SadConsole.Console {
 
         private ConsoleManager conMan;
         private int selectChoice = 0;
-        private string currentRace = "You have not selected a race yet.";
+        static public string currentRace = "You have not selected a race yet.";
 
         public CharacterCreate(ConsoleManager conMan) : base(80, 25) {
             this.conMan = conMan;
-            Print(33, 0, "Character Create", Color.White, Color.Black);
-            Print(33, 24, "Press space to play", Color.Black, Color.PeachPuff);
         }
 
-        public override void Update(TimeSpan timeElapsed) {
+         public override void Update(TimeSpan timeElapsed) {
+            Clear();
             DrawMenuOptions();
             CheckInput();
             base.Update(timeElapsed);
         }
 
         private void DrawMenuOptions() {
+            Print(28, 4, "Character Create", Color.White, Color.Black);
+            Print(28, 22, "Press enter to select, space to play", Color.Black, Color.PeachPuff);
             if (selectChoice == 0) {
-                Print(38, 10, "Human", Color.Red);
-                Print(38, 12, "Kimi", Color.White);
-                Print(38, 14, "High Uvese", Color.White);
-                Print(38, 16, "Bronk", Color.White);
-                Print(38, 18, currentRace, Color.PeachPuff);
+                Print(34, 8, "Human", Color.Red);
+                Print(34, 10, "Kimi", Color.White);
+                Print(34, 12, "High Uvese", Color.White);
+                Print(34, 14, "Bronk", Color.White);
+                Print(28, 18, currentRace, Color.PeachPuff);
             }
             else if (selectChoice == 1) {
-                Print(38, 10, "Human", Color.White);
-                Print(38, 12, "Kimi", Color.Red);
-                Print(38, 14, "High Uvese", Color.White);
-                Print(38, 16, "Bronk", Color.White);
-                Print(38, 18, currentRace, Color.PeachPuff);
+                Print(34, 8, "Human", Color.White);
+                Print(34, 10, "Kimi", Color.Red);
+                Print(34, 12, "High Uvese", Color.White);
+                Print(34, 14, "Bronk", Color.White);
+                Print(28, 18, currentRace, Color.PeachPuff);
 
             }
             if (selectChoice == 2) {
-                Print(38, 10, "Human", Color.White);
-                Print(38, 12, "Kimi", Color.White);
-                Print(38, 14, "High Uvese", Color.Red);
-                Print(38, 16, "Bronk", Color.White);
-                Print(38, 18, currentRace, Color.PeachPuff);
+                Print(34, 8, "Human", Color.White);
+                Print(34, 10, "Kimi", Color.White);
+                Print(34, 12, "High Uvese", Color.Red);
+                Print(34, 14, "Bronk", Color.White);
+                Print(28, 18, currentRace, Color.PeachPuff);
 
             }
             else if (selectChoice == 3) {
-                Print(38, 10, "Human", Color.White);
-                Print(38, 12, "Kimi", Color.White);
-                Print(38, 14, "High Uvese", Color.White);
-                Print(38, 16, "Bronk", Color.Red);
-                Print(38, 18, currentRace, Color.PeachPuff);
+                Print(34, 8, "Human", Color.White);
+                Print(34, 10, "Kimi", Color.White);
+                Print(34, 12, "High Uvese", Color.White);
+                Print(34, 14, "Bronk", Color.Red);
+                Print(28, 18, currentRace, Color.PeachPuff);
 
             }
         }
